27eba0e61afe8fbc02abf9c87e362a4804d4320b
Ricardo <Riickyluja19@gmail.com>
2022-06-19 17:40:20 -0300

    v1
    v1

--- crear_tablas.sql
+++ crear_tablas.sql
@@ -10,19 +10,23 @@ CREATE TABLE tabla_completa (
     Codigo_postal text,
     Número_de_teléfono text,
     Mail text,
-    Web text
+    Web text,
+    Fecha_y_hora text
     );
 
 CREATE TABLE datos_salas_de_cine (
     Provincia text,
     Pantallas text,
     Butacas text,
-    espacio_INCAA text
+    espacio_INCAA text,
+    Fecha_y_hora text
 );
 
  
 CREATE TABLE registros_totoales(
-    total_por_categoria text,
-    totoal_por_fuente text,
-    provincia_y_categoria text
+    Total_por_categoria text,
+    Totoal_por_fuente text,
+    Provincia_y_categoria text,
+    Fecha_y_hora text
+
 );
